297c980a422619fbd9e9ff434d5adbe9d29eaf38
AnjaliKiran <55487588+AnjaliKiran@users.noreply.github.com>
2019-11-06 23:52:50 +0530

    Add files via upload

---
+++ exp 3 code.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+  pinMode(2,OUTPUT);
+  pinMode(3,INPUT);
+}
+void loop()
+{
+  int a=digitalRead(3);
+  if(a==HIGH)
+  {
+    digitalWrite(2,HIGH);
+  }
+  else
+  {
+    digitalWrite(3,LOW);
+  }
+}
